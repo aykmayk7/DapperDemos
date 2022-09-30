af89b35e742b8c78a67bc51874f432933bc41d06
Aykan CESUR <aykancesur@outlook.com>
2022-09-30 16:02:11 +0300

    Add project files.

---
+++ HelperLibrary/Tools.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+namespace HelperLibrary
+{
+    public static class Tools
+    {
+        public static string GetConnectionString(string name = "DapperDB")
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
